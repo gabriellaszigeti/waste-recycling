ac2b514ee154d9b318037f5097b4e925a1f99313
gabriellaszigeti <gbszgt@gmail.com>
2021-10-07 13:24:53 +0100

    feat : create PaperGarbage subclass

--- src/Codecool.WasteRecycling/PaperGarbage.cs
+++ src/Codecool.WasteRecycling/PaperGarbage.cs
@@ -2,5 +2,23 @@ namespace Codecool.WasteRecycling
 {
     public class PaperGarbage : Garbage
     {
+
+        public bool Squeezed { get; private set; }
+
+        public PaperGarbage(string name,bool squeezed) : base(name)
+        {
+            Squeezed = squeezed;
+        }
+
+        public void Squeeze()
+        {
+            this.Squeezed = true;
+        }
+
+        public override string ToString()
+        {
+            return $"{{{nameof(Squeezed)}={Squeezed.ToString()}, {nameof(Name)}={Name}}}";
+        }
     }
+
 }
