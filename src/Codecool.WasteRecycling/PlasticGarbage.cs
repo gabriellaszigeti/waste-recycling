780517d550da6aafd6c81a0e97db29486c2efee5
gabriellaszigeti <gbszgt@gmail.com>
2021-10-07 13:25:19 +0100

    feat : create PlasticGarbage subclass

--- src/Codecool.WasteRecycling/PlasticGarbage.cs
+++ src/Codecool.WasteRecycling/PlasticGarbage.cs
@@ -2,5 +2,23 @@ namespace Codecool.WasteRecycling
 {
     public class PlasticGarbage : Garbage
     {
+
+        public bool Cleaned { get; private set; }
+
+        public PlasticGarbage(string name, bool cleaned) : base(name)
+        {
+            Cleaned = cleaned;
+        }
+
+
+        public  void Clean()
+        {
+            this.Cleaned = true;
+        }
+
+        public override string ToString()
+        {
+            return $"{{{nameof(Cleaned)}={Cleaned.ToString()}, {nameof(Name)}={Name}}}";
+        }
     }
 }
