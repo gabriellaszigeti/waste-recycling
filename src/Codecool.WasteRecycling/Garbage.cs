404e30192eeb61fe18de4807c3ad8606f53a71bf
gabriellaszigeti <gbszgt@gmail.com>
2021-10-07 13:24:14 +0100

    feat : create Garbage class

--- src/Codecool.WasteRecycling/Garbage.cs
+++ src/Codecool.WasteRecycling/Garbage.cs
@@ -2,5 +2,21 @@ namespace Codecool.WasteRecycling
 {
     public class Garbage
     {
+
+        public string Name { get; }
+
+        public Garbage(string name)
+        {
+            Name = name;
+        }
+
+
+
+        public override string ToString()
+        {
+            return $"{{{nameof(Name)}={Name}}}";
+        }
+
+  
     }
-}
+    }
